07ab8faa9cdb8816a648a0cfec92508305ba2ca7
Muhammad Khizer Kamal <khizerkamal.03@gmail.com>
2021-09-13 19:36:33 +0500

    LinkedList 2

--- # 22 LinkedList/LL.cpp
+++ # 22 LinkedList/LL.cpp
@@ -49,6 +49,34 @@ bool linearSearch(node * head, int key){
     return false;
 }
 
+void deletion(node* &head, int val){
+    // Corner Cases
+    if(head == NULL){
+        return;
+    }
+
+    if(head->next == NULL){
+        deleteAtHead(head);
+        return;
+    }
+    
+    node *temp = head;
+
+    while(temp->next->data != val){
+        temp = temp->next;
+    }
+
+    node *todelete = temp->next;
+    temp->next = temp->next->next;
+    delete todelete;
+}
+
+void deleteAtHead(node* &head){
+    node *todelete = head;
+    head = head->next;
+    delete todelete;
+}
+
 void display(node* head){
     node *temp = head;
     while(temp != NULL){
@@ -65,9 +93,13 @@ int main(){
     insertAtTail(head, 3);
     insertAtTail(head, 4);
     display(head);
-    insertAtHead(head, 4);
+    // insertAtHead(head, 4);
+    // display(head);
+    // cout << linearSearch(head, 2) << endl;
+    // deletion(head, 2);
+    // display(head);
+    deleteAtHead(head);
     display(head);
-    cout << linearSearch(head, 2) << endl;
 
     return 0;
 }
